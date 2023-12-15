b4b8a999e39729e7d8ac539e95199dbaee4c1c51
man-coding <chch2857@gmail.com>
2023-12-15 09:10:18 +0900

    cote

--- src/stream/test.java
+++ src/stream/test.java
@@ -20,15 +20,25 @@ public class test {
 		int max = stream.sorted(Comparator.reverseOrder()).limit(2).reduce(1, (a, b) -> a * b);
 
 		System.out.println(max);
+		
+		
 
 		//인트스트림으로 풀기
 		
 		int[] arr = { 1, 2, 3, 4, 5 };
 
 		IntStream stream2 = Arrays.stream(arr);
-		int max2 = stream2.boxed().sorted(Comparator.reverseOrder()).limit(2).reduce(1, (a, b) -> a * b);
-
+		int max2 = stream2 
+					.boxed()
+					.sorted(Comparator.reverseOrder())
+					.limit(2)
+					.reduce(1, (a, b) -> a * b);
 		System.out.println(max2);
+//map, mapToInt, boxed() 세 개가 다 다르다  
+//		map: Stream<> -> Stream<?>		   //stream과 intstream 은 리스트 혹은 배열 등 데이터를 카피한 것이라고 생각하면 된다.
+//		mapToInt: Stream -> IntStream		//둘 다 형태는 비슷하나 intstream은 숫자에 대한 함수들을 사용할 수 있다는 점에서 다르다.
+//		boxed: IntStream -> Stream
+		
 
 		
 	}
